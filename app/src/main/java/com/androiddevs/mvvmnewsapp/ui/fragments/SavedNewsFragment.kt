79a29dd8bb150be7dbe3af2771baff04c85e78c9
vaibhavvaish1 <52125485+vaibhavvaish1@users.noreply.github.com>
2024-12-09 17:22:38 +0530

    initial architecture

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
@@ -0,0 +1,14 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.androiddevs.mvvmnewsapp.R
+
+
+class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
+
+
+}
