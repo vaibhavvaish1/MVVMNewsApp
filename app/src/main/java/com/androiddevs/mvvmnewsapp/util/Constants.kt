fa2576aa486ad34e54f129e7d9cae28d478b0239
vaibhavvaish1 <52125485+vaibhavvaish1@users.noreply.github.com>
2024-12-10 15:19:21 +0530

    Add retrofit for http request and response

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "63bd794508f24c1d88164a7a06453585"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
